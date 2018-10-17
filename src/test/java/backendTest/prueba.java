62c8d903c0ea06ea02fd2efa696f257f50f683de
Dova81 <33400774+Dova81@users.noreply.github.com>
2018-10-17 12:22:38 -0300

    test prueba

---
+++ src/test/java/backendTest/prueba.java
@@ -0,0 +1,15 @@
+package backendTest;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class prueba {
+
+    @Test
+    public void pruebas(){
+
+        assertTrue(true);
+    }
+
+}
